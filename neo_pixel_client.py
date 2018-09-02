dbbe63b4dfc040ea853b4e9cc32669441104d346
ayanez17 <fredyyanez39@yahoo.com>
2018-09-01 21:04:20 -0400

    cleaning up code

--- neo_pixel_client.py
+++ neo_pixel_client.py
@@ -11,8 +11,8 @@ import time
 LED_COUNT      = 16      # Number of LED pixels.
 LED_PIN        = 12      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_PIN2       = 13      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_PIN3       = 18
-LED_PIN4       = 19
+# LED_PIN3       = 18
+# LED_PIN4       = 19
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
@@ -28,58 +28,40 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     global strip1
     global strip2
-    global strip3
-    global strip4
+    # global strip3
+    # global strip4
     strip1 = Adafruit_NeoPixel(LED_COUNT, 12, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, 0)
     strip2 = Adafruit_NeoPixel(LED_COUNT, 13, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, 1)
-    strip3 = Adafruit_NeoPixel(LED_COUNT, 18, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, 0)
-    strip4 = Adafruit_NeoPixel(LED_COUNT, 19, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, 1)
+    # strip3 = Adafruit_NeoPixel(LED_COUNT, 18, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, 0)
+    # strip4 = Adafruit_NeoPixel(LED_COUNT, 19, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, 1)
     strip1.begin()
     strip2.begin()
-    strip3.begin()
-    strip4.begin()
+    # strip3.begin()
+    # strip4.begin()
     #test
-    client.subscribe('test/one')
-    client.subscribe('test/two')
-    client.subscribe('test/three')
-    client.subscribe('test/four')
+    client.subscribe('test/#')
+
         #Light1
-    client.subscribe("light1/report/status")
-    client.subscribe("light1/status")
-    client.subscribe("light1/report/brightness")
-    client.subscribe("light1/brightness")
-    client.subscribe("light1/report/hue")
-    client.subscribe("light1/hue")
-    client.subscribe("light1/report/saturation")
-    client.subscribe("light1/saturation")
+    client.subscribe("light1/#")
+    # client.subscribe("light1/report/status")
+    # client.subscribe("light1/status")
+    # client.subscribe("light1/report/brightness")
+    # client.subscribe("light1/brightness")
+    # client.subscribe("light1/report/hue")
+    # client.subscribe("light1/hue")
+    # client.subscribe("light1/report/saturation")
+    # client.subscribe("light1/saturation")
         #Light2
-    client.subscribe("light2/report/status")
-    client.subscribe("light2/status")
-    client.subscribe("light2/report/brightness")
-    client.subscribe("light2/brightness")
-    client.subscribe("light2/report/hue")
-    client.subscribe("light2/hue")
-    client.subscribe("light2/report/saturation")
-    client.subscribe("light2/saturation")
-        #Light3
-    client.subscribe("light3/report/status")
-    client.subscribe("light3/status")
-    client.subscribe("light3/report/brightness")
-    client.subscribe("light3/brightness")
-    client.subscribe("light3/report/hue")
-    client.subscribe("light3/hue")
-    client.subscribe("light3/report/saturation")
-    client.subscribe("light3/saturation")
-        #Light4
-    client.subscribe("light4/report/status")
-    client.subscribe("light4/status")
-    client.subscribe("light4/report/brightness")
-    client.subscribe("light4/brightness")
-    client.subscribe("light4/report/hue")
-    client.subscribe("light4/hue")
-    client.subscribe("light4/report/saturation")
-    client.subscribe("light4/saturation")
-# The callback for when a PUBLISH message is received from the server.
+    client.subscribe("light2/#")
+    # client.subscribe("light2/report/status")
+    # client.subscribe("light2/status")
+    # client.subscribe("light2/report/brightness")
+    # client.subscribe("light2/brightness")
+    # client.subscribe("light2/report/hue")
+    # client.subscribe("light2/hue")
+    # client.subscribe("light2/report/saturation")
+    # client.subscribe("light2/saturation")
+
 def colorWipe(strip, color, wait_ms=50):
     """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
@@ -90,16 +72,16 @@ def light_status(msg, strip, rgb_index, all_rgb):
     if msg.payload == "true":
         print('ON status ', rgb_index)
         color = Color(all_rgb[rgb_index][4], all_rgb[rgb_index][3],all_rgb[rgb_index][5])
-	if all_rgb[rgb_index][2] == 0:
+    if all_rgb[rgb_index][2] == 0:
             all_rgb[rgb_index][2]= 100
         strip.setBrightness(all_rgb[rgb_index][2])
-	colorWipe(strip,color)
+    colorWipe(strip,color)
 #        for i in range(strip.numPixels()):
 #            strip.setPixelColor(i,color)
 #            strip.show()
     else:
         all_rgb[rgb_index][2] = 0
-	strip.setBrightness(all_rgb[rgb_index][2])
+    strip.setBrightness(all_rgb[rgb_index][2])
         strip.show()
 def brightness(msg, strip, rgb_index, all_rgb):
     print('inside bts ', rgb_index)
@@ -125,8 +107,8 @@ def saturation(msg, strip, rgb_index, all_rgb):
         all_rgb[rgb_index][5] = int(c2[2]*255)
         color = Color(all_rgb[rgb_index][4],all_rgb[rgb_index][3],all_rgb[rgb_index][5])
         colorWipe(strip,color)
-	print(color, 'wipe')
-	#for i in range(strip.numPixels()):
+    print(color, 'wipe')
+    #for i in range(strip.numPixels()):
         #    strip.setPixelColor(i, color)
         #strip.show()
 def on_message(client, userdata, msg):
@@ -134,21 +116,15 @@ def on_message(client, userdata, msg):
     global rgbs
     print(rgbs)
     if msg.topic =="light1/status" :
-	print('light1')
-	print(str(strip1))
-	print(vars(strip1))
-	print(strip1._led_data.channel)
-	print(dir(strip1._channel))
-	print(strip1._channel.__class__)
         light_status(msg,strip1,0,rgbs)
     if msg.topic =="light2/status" :
         print('light2')
         light_status(msg,strip2,1,rgbs)
     if msg.topic =="light3/status" :
-	print('light3')
+    print('light3')
         light_status(msg,strip3,2,rgbs)
     if msg.topic =="light4/status" :
-	print('light4')
+    print('light4')
         light_status(msg,strip4,3,rgbs)
 
     if msg.topic =="light1/brightness":
@@ -177,9 +153,9 @@ def on_message(client, userdata, msg):
         hue(msg,strip3,2,rgbs)
     if msg.topic == "light4/hue":
         hue(msg,strip4,3,rgbs)
-	#TEST
+    #TEST
     if msg.topic == "test/one":
-	colorWipe(strip1, Color(0, 255, 0))
+    colorWipe(strip1, Color(0, 255, 0))
     if msg.topic == "test/two":
         colorWipe(strip2, Color(0, 255, 0))
     if msg.topic == "test/three":
@@ -188,12 +164,13 @@ def on_message(client, userdata, msg):
         colorWipe(strip4, Color(0, 255, 0))
     print(rgbs)
 
+#client = mqtt.Client("", True, None, mqtt.MQTTv31)
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("localhost", 1883, 60)
-
+#client = mqtt.Client("", True, None, mqtt.MQTTv31)
 # Process network traffic and dispatch callbacks. This will also handle
 # reconnecting. Check the documentation at
 # https://github.com/eclipse/paho.mqtt.python
