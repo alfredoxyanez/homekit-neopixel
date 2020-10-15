773f77cba563415fa8571dc78b0975edce2a484a
alfredo-vive <alfredo@vivebenefits.com>
2020-10-15 13:31:37 -0400

    updated client v2

--- neo_pixel_client_v2.py
+++ neo_pixel_client_v2.py
@@ -1,76 +1,116 @@
 from neopixel import *
 import board
-import time
+import paho.mqtt.client as mqtt
+import threading
+import colorsys
 
-# The callback for when the client receives a CONNACK response from the server.
 
-global light_1
-pixel_pin = board.D18
+global pixels_1
+global pixels_2
+pixel_pin_1 = board.D18
+pixel_pin_2 = board.D19
 ORDER = GRB
 # The number of NeoPixels
 num_pixels = 16
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 
-pixels = NeoPixel(
-    pixel_pin, num_pixels, brightness=0.2, auto_write=False,
+pixels_1 = NeoPixel(
+    pixel_pin_1, num_pixels, brightness=0.2, auto_write=False,
+    pixel_order=ORDER
+)
+
+pixels_2 = NeoPixel(
+    pixel_pin_2, num_pixels, brightness=0.2, auto_write=False,
     pixel_order=ORDER
 )
 
 
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
+def light_status(msg, strip, rgb_index, all_rgb):
+    if msg.payload == "true":
+        color = (all_rgb[rgb_index][3],
+                 all_rgb[rgb_index][4],
+                 all_rgb[rgb_index][5])
+        if all_rgb[rgb_index][2] == 0:
+            all_rgb[rgb_index][2] = 100
+        strip.setBrightness(all_rgb[rgb_index][2])
+        strip.fill(color)
+        strip.show()
     else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-    return (r, g, b) if ORDER in (RGB, GRB) else (r, g, b, 0)
+        all_rgb[rgb_index][2] = 0
+        strip.setBrightness(all_rgb[rgb_index][2])
+        strip.show()
 
 
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(pixel_index & 255)
-        pixels.show()
-        time.sleep(wait)
+def brightness(msg, strip, rgb_index, all_rgb):
+    bn = int(msg.payload)
+    bn = int(255 * bn * .01)
+    all_rgb[rgb_index][2] = bn
+    strip.setBrightness(all_rgb[rgb_index][2])
+    strip.show()
 
 
-while True:
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((255, 0, 0))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((255, 0, 0, 0))
-    pixels.show()
-    time.sleep(1)
-
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 255, 0))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 255, 0, 0))
-    pixels.show()
-    time.sleep(1)
-
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 0, 255))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 0, 255, 0))
-    pixels.show()
-    time.sleep(1)
-
-    rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
+def hue(msg, strip, rgb_index, all_rgb):
+    all_rgb[rgb_index][1] = int(msg.payload) / 360.0
+    c = colorsys.hls_to_rgb(all_rgb[rgb_index][1], .5, all_rgb[rgb_index][0])
+    all_rgb[rgb_index][3] = int(c[0] * 255)
+    all_rgb[rgb_index][4] = int(c[1] * 255)
+    all_rgb[rgb_index][5] = int(c[2] * 255)
+    color = (all_rgb[rgb_index][4],
+             all_rgb[rgb_index][3],
+             all_rgb[rgb_index][5])
+    strip.fill(color)
 
 
+def saturation(msg, strip, rgb_index, all_rgb):
+    all_rgb[rgb_index][0] = int(msg.payload) * .01
+    c2 = colorsys.hls_to_rgb(all_rgb[rgb_index][1], .5, all_rgb[rgb_index][0])
+    all_rgb[rgb_index][3] = int(c2[0] * 255)
+    all_rgb[rgb_index][4] = int(c2[1] * 255)
+    all_rgb[rgb_index][5] = int(c2[2] * 255)
+    color = (all_rgb[rgb_index][4], all_rgb[rgb_index][3],
+                  all_rgb[rgb_index][5])
+    strip.fill(color)
+
+
+def on_connect(client, userdata, flags, rc):
+    # Light1
+    client.subscribe("light1/#")
+    # Light2
+    client.subscribe("light2/#")
+
+def on_message(client, userdata, msg):
+    global rgbs
+    if msg.topic == "light1/status":
+        t = threading.Thread(target=light_status, args=(msg, pixels_1, 0, rgbs))
+        t.start()
+    if msg.topic == "light2/status":
+        t = threading.Thread(target=light_status, args=(msg, pixels_2, 1, rgbs))
+        t.start()
+
+    if msg.topic == "light1/brightness":
+        t = threading.Thread(target=brightness, args=(msg, pixels_1, 0, rgbs))
+        t.start()
+    if msg.topic == "light2/brightness":
+        t = threading.Thread(target=brightness, args=(msg, pixels_2, 1, rgbs))
+        t.start()
+
+    if msg.topic == "light1/saturation":
+        t = threading.Thread(target=saturation, args=(msg, pixels_1, 0, rgbs))
+        t.start()
+    if msg.topic == "light2/saturation":
+        t = threading.Thread(target=saturation, args=(msg, pixels_2, 1, rgbs))
+        t.start()
+
+    if msg.topic == "light1/hue":
+        t = threading.Thread(target=hue, args=(msg, pixels_1, 0, rgbs))
+        t.start()
+    if msg.topic == "light2/hue":
+        t = threading.Thread(target=hue, args=(msg, pixels_2, 1, rgbs))
+        t.start()
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+client.loop_forever()
