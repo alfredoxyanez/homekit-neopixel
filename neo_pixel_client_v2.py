976edff4b92369c28aafbd0668589b205240da7a
alfredo-vive <alfredo@vivebenefits.com>
2020-10-15 14:55:42 -0400

    updated client v2

--- neo_pixel_client_v2.py
+++ neo_pixel_client_v2.py
@@ -15,7 +15,7 @@ ORDER = GRB
 num_pixels = 16
 
 pixels_1 = NeoPixel(
-    pixel_pin_1, num_pixels, brightness=0.2, auto_write=False,
+    pixel_pin_1, num_pixels, brightness=.5, auto_write=False,
     pixel_order=ORDER
 )
 
@@ -23,13 +23,15 @@ pixels_1 = NeoPixel(
 #     pixel_pin_2, num_pixels, brightness=0.2, auto_write=False,
 #     pixel_order=ORDER
 # )
-# [Saturation, , Brightness, G, R, B]
-light_status = [0, 0, 100, 255, 255, 255]
+# [Saturation, , Brightness (0,1), G, R, B]
+light_status = [0, 0, .5, 255, 255, 255]
 
 
 def status(msg):
+    import pdb;
+    pdb.set_trace()
     if msg.payload == "true":
-        light_status[2] = 100 if light_status[2] == 0 else light_status[2]
+        light_status[2] = 1 if light_status[2] == 0 else light_status[2]
     else:
         light_status[2] = 0
 
@@ -40,6 +42,8 @@ def status(msg):
 
 
 def brightness(msg):
+    import pdb;
+    pdb.set_trace()
     bn = int(msg.payload)
     bn = int(255 * bn * .01)
     light_status[2] = bn
@@ -50,6 +54,8 @@ def brightness(msg):
 
 
 def hue(msg, strip, rgb_index, all_rgb):
+    import pdb;
+    pdb.set_trace()
     all_rgb[rgb_index][1] = int(msg.payload) / 360.0
     c = colorsys.hls_to_rgb(all_rgb[rgb_index][1], .5, all_rgb[rgb_index][0])
     all_rgb[rgb_index][3] = int(c[0] * 255)
@@ -62,6 +68,8 @@ def hue(msg, strip, rgb_index, all_rgb):
 
 
 def saturation(msg, strip, rgb_index, all_rgb):
+    import pdb;
+    pdb.set_trace()
     all_rgb[rgb_index][0] = int(msg.payload) * .01
     c2 = colorsys.hls_to_rgb(all_rgb[rgb_index][1], .5, all_rgb[rgb_index][0])
     all_rgb[rgb_index][3] = int(c2[0] * 255)
